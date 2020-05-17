3aeb076244dcadf3cf1bdfbede71827a194cdb24
Adan Rodriguez <adanrodriguez@Kourtneys-MBP.hitronhub.home>
2020-05-17 13:42:40 -0500

    holy crap i forgot to commit a lot... ummm started with just the directions in the read me. Mapped over items, worked through some errors, and then started building out styles that I liked

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import Header from "./components/Header.js";
+import {Route} from 'react-router-dom';
+import WelcomePage from "./components/WelcomePage.js";
+import CharacterList from "./components/CharacterList";
+import SearchForm from './components/SearchForm';
 
 export default function App() {
   return (
     <main data-testid='app'>
       <Header />
+      <Route path = '/search' component = {SearchForm}/>
+      <Route exact path = '/'component = {WelcomePage}/>
+      <Route path = '/characters'component = {CharacterList}/>
     </main>
   );
 }
